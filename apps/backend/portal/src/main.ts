41d01c4d3501d22273cb2c6c255f32c130c1960c
rj <rj@oxyguard.dk>
2025-04-09 18:04:53 +0200

    feat: host frontend and assets

--- apps/backend/portal/src/main.ts
+++ apps/backend/portal/src/main.ts
@@ -1,9 +1,31 @@
 import { Elysia } from "elysia";
+import { $ } from "bun";
 
+// * Host the frontend and static resources 
+try {
+  await $`HOST=0.0.0.0 PORT=3001 bunx run ../../../dist/apps/frontend/portal/server/entry.mjs`;
+} catch {
+  console.error("Server might be runnning");
+}
+
+// * Host the api
 const app = new Elysia()
   .get("/api/", () => "Hello Elysia")
-  .listen(3000);
+  .get('/api/ping', () => 'pong')
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  // * Proxy all other requests to the frontend server
+  .get('/*', async ({ request }) => {
+    const original = new URL(request.url);
+    const proxyUrl = `http://localhost:3001${original.pathname}`;
+
+    return await fetch(proxyUrl, {
+      headers: {
+        ...request.headers,
+      },
+    });
+  })
+
+  .listen(3_000);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+
--- apps/frontend/portal/astro.config.mjs
+++ apps/frontend/portal/astro.config.mjs
@@ -4,9 +4,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import proxy from './proxy.conf.json' assert { type: 'json' };
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    outDir: '../../../dist/apps/frontend/portal',
+    adapter: node({
+        mode: 'standalone',
+    }),
     site: 'https://example.com',
     integrations: [
         mdx(),
