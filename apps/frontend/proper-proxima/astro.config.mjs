c92f1111324c827b42146c9b3c74ae3ab580ed63
rj <rj@oxyguard.dk>
2025-04-09 13:35:41 +0200

    feat: create api call from Svelte, and display result

--- apps/frontend/proper-proxima/astro.config.mjs
+++ apps/frontend/proper-proxima/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
+import proxy from './proxy.conf.json' assert { type: 'json' };
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,11 @@ export default defineConfig({
         sitemap(),
         svelte(),
     ],
+
+    // Add Vite configuration with proxy settings
+    vite: {
+        server: {
+            proxy,
+        }
+    }
 });
