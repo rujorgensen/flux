7bd483284f7054937cc36ad5a94ba7399f82fc82
rj <rj@oxyguard.dk>
2025-04-17 14:23:15 +0200

    fix

--- apps/frontend/portal/src/components/dashboard/ConnectedAgents.svelte
+++ apps/frontend/portal/src/components/dashboard/ConnectedAgents.svelte
@@ -1,8 +1,49 @@
 <!-- ConnectedAuthorities.svelte -->
 <script lang="ts">
-    import { connectedAgents } from "../../business-logic/connected-agents.fn";
+  import { writable } from 'svelte/store';
+  import { apiFetch } from '../../utils/fetch.util';
+
+  // import { onMount } from "svelte";
+
+  const dataStore = writable<number | undefined>();
+
+  // dataStore.set();
+  let val = 0;
+  // Fetch initial data
+  apiFetch('api/ping')
+    .then((res) => res.text())
+    .then((initialData) => {
+      console.log('[initialData]', initialData);
+      val = 23;
+      dataStore.set(val);
+    })
+    .catch((err) => {
+      console.error('Error fetching initial data:', err);
+      // dataStore.set({ error: 'Failed to fetch initial data' });
+    });
+
+  setInterval(() => {
+    dataStore.set(val++);
+  }, 1_000);
+  // onMount(async () => {
+  //   // const socket = new WebSocket('wss://example.com');
+
+  //   // socket.addEventListener('message', (event) => {
+  //   //   const newData = JSON.parse(event.data);
+
+  //   //   dataStore.update(current => ({
+  //   //     ...current,
+  //   //     ...newData
+  //   //   }));
+  //   // });
+
+  //   //    return () => socket.close();
+  // });
+
+  // Export the store directly
+  export const connectedAgents = dataStore;
 </script>
 
 {#if $connectedAgents}
-    <strong>{JSON.stringify($connectedAgents)}</strong>
+  <strong>{JSON.stringify($connectedAgents)}</strong>
 {/if}
--- apps/frontend/portal/src/components/dashboard/ConnectedAuthorities.svelte
+++ apps/frontend/portal/src/components/dashboard/ConnectedAuthorities.svelte
@@ -1,8 +1,45 @@
 <!-- ConnectedAuthorities.svelte -->
 <script lang="ts">
-    import { connectedAuthorities } from "../../business-logic/connected-authorities.fn";
+  import { writable } from 'svelte/store';
+  import { FluxAgent } from '../../../../../../packages/flux/agent/src';
+  import type {
+    FluxNetworkChannel,
+    FluxNetworkConnection,
+  } from '../../../../../../libs/flux/shared/connection/src';
+  import { onMount } from 'svelte';
+
+  // Passed by Astro
+  export let initialData: number;
+
+  const CODE_TO_ACCESS_NETWORK: string = 'code-to-access-network'; // Key to connect to a network, unknown and irelevant to flux
+
+  const dataStore = writable<number | undefined>();
+
+  dataStore.set(initialData ?? 0);
+
+  onMount(async () => {
+    // if (isBrowser()) {
+    const fluxAgent = new FluxAgent('network-id', {
+      //         domain?: string,
+      //         secretKey?: string; // For encrypting/decrypting packages. Not known to Flux.
+      //         retries?: number; // Number of times to retry a failed message
+    });
+
+    const fluxNetworkConnection: FluxNetworkConnection =
+      await fluxAgent.connect(CODE_TO_ACCESS_NETWORK, 'portal-agent');
+
+    const fluxNetworkChannel: FluxNetworkChannel =
+      await fluxNetworkConnection.joinChannel('connected-authorities');
+
+    fluxNetworkChannel.onPublish((message: string) => {
+      dataStore.set(Number.parseInt(message, 10));
+    });
+    //  }
+  });
+  // Export the store directly
+  export const connectedAuthorities = dataStore;
 </script>
 
 {#if $connectedAuthorities}
-    <strong>{JSON.stringify($connectedAuthorities)}</strong>
+  <strong>{JSON.stringify($connectedAuthorities)}</strong>
 {/if}
