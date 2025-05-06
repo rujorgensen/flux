8b86851bbaaefd7efdb563e22246fa4c24cd8f31
rj <rj@oxyguard.dk>
2025-05-06 11:21:53 +0200

    feat: add logging library

---
+++ libs/utils/logger/src/lib/utils-logger.ts
@@ -0,0 +1,53 @@
+const hasOverlap = <T>(a: Set<T> | undefined, b?: T[] | T) => a && b ? [...a].some((item: T) => (Array.isArray(b) ? b : [b]).includes(item)) : false;
+
+export const PicoLogger = (() => {
+    let allowScopes: Set<string> | undefined;
+
+    const configure = (
+        configuration: {
+            allowScopes: string[];
+        },
+    ) => {
+        allowScopes = new Set(configuration.allowScopes);
+    };
+
+    const log = (
+        message: string,
+        scopes?: string | string[],
+    ): void => {
+        if (!hasOverlap(allowScopes, scopes)) {
+            return;
+        }
+
+        console.log(message);
+    };
+
+    const warn = (
+        message: string,
+        scopes?: string | string[],
+    ): void => {
+        if (!hasOverlap(allowScopes, scopes)) {
+            return;
+        }
+
+        console.warn(message);
+    };
+
+    const error = (
+        message: string,
+        scopes?: string | string[],
+    ): void => {
+        if (!hasOverlap(allowScopes, scopes)) {
+            return;
+        }
+
+        console.error(message);
+    };
+
+    return {
+        configure,
+        log,
+        warn,
+        error,
+    };
+})();
