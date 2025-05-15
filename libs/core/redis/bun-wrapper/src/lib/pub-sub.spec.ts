8358252a37fdd2c6a7409792ec2486f07d10c9c1
rj <rj@oxyguard.dk>
2025-05-15 15:07:32 +0200

    format

--- libs/core/redis/bun-wrapper/src/lib/pub-sub.spec.ts
+++ libs/core/redis/bun-wrapper/src/lib/pub-sub.spec.ts
@@ -1,67 +1,67 @@
 import {
-  type RedisClientType,
-  createClient,
+    type RedisClientType,
+    createClient,
 } from 'redis';
 import {
-  type StartedRedisContainer,
-  RedisContainer,
+    type StartedRedisContainer,
+    RedisContainer,
 } from '@testcontainers/redis';
-import { BunRedisPubSub } from './core-redis-pub-sub.class'
+import { BunRedisPubSub } from './core-redis-pub-sub.class';
 import {
-  Wait,
+    Wait,
 } from 'testcontainers';
 
 describe('BunRedisPubSub', () => {
-  let redisContainer: StartedRedisContainer;
-  let redisClient: RedisClientType;
-  let pubsub: BunRedisPubSub;
+    let redisContainer: StartedRedisContainer;
+    let redisClient: RedisClientType;
+    let pubsub: BunRedisPubSub;
 
-  beforeAll(async () => {
-    redisContainer = await new RedisContainer('redis:6.2.7')
-      .withExposedPorts(6379)
-      .withWaitStrategy(Wait.forLogMessage('Ready to accept connections'))
-      .start();
+    beforeAll(async () => {
+        redisContainer = await new RedisContainer('redis:6.2.7')
+            .withExposedPorts(6379)
+            .withWaitStrategy(Wait.forLogMessage('Ready to accept connections'))
+            .start();
 
-    const url: string = redisContainer.getConnectionUrl();
+        const url: string = redisContainer.getConnectionUrl();
 
-    console.log(`Redis is ready at ${url}`)
+        console.log(`Redis is ready at ${url}`);
 
-    redisClient = createClient({
-      url,
+        redisClient = createClient({
+            url,
+        });
+
+        pubsub = new BunRedisPubSub({
+            url,
+            socket: {
+                reconnectStrategy: () => 100,
+            },
+        });
+
+        await pubsub.connect();
+        await redisClient.connect();
     });
 
-    pubsub = new BunRedisPubSub({
-      url,
-      socket: {
-        reconnectStrategy: () => 100,
-      },
+    afterAll(async () => {
+        await redisClient?.disconnect();
+        await pubsub?.disconnect();
+        await redisContainer?.stop();
     });
 
-    await pubsub.connect();
-    await redisClient.connect();
-  });
+    it('works', async () => {
+        await redisClient.set('key', 'val');
+        expect(await redisClient.get('key')).toBe('val');
+    });
 
-  afterAll(async () => {
-    await redisClient?.disconnect();
-    await pubsub?.disconnect();
-    await redisContainer?.stop();
-  });
+    it('should publish and receive messages', async () => {
+        const messages: string[] = [];
 
-  it('works', async () => {
-    await redisClient.set('key', 'val');
-    expect(await redisClient.get('key')).toBe('val');
-  });
+        await pubsub.subscribe('test-channel', messages.push.bind(messages));
 
-  it('should publish and receive messages', async () => {
-    const messages: string[] = []
+        await pubsub.publish('test-channel', 'hello world');
 
-    await pubsub.subscribe('test-channel', messages.push.bind(messages))
+        await new Promise((resolve) => setTimeout(resolve, 100));
 
-    await pubsub.publish('test-channel', 'hello world')
-
-    await new Promise((resolve) => setTimeout(resolve, 100));
-
-    expect(messages).toContain('hello world')
-  })
+        expect(messages).toContain('hello world');
+    });
 
 });
