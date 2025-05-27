0b627dba8486d894e858743be42927e9134d9262
rj <rj@oxyguard.dk>
2025-05-27 11:20:20 +0200

    .

--- libs/flux/shared/types/src/lib/auth/auth.types.ts
+++ libs/flux/shared/types/src/lib/auth/auth.types.ts
@@ -1,4 +1,4 @@
-import type { TNetworkId_S } from '../flux-shared';
+import type { TNetworkId_S } from '@flux/shared/types';
 
 /**
  * Ticket returned by the authentication service. Pass this ticket when connecting the websocket.
