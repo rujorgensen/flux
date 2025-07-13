3021e33447b4e4b04284365d15d8034ca5caa1fc
rj <rj@oxyguard.dk>
2025-07-13 11:57:56 +0200

    chore: auto format

--- run-stack.ts
+++ run-stack.ts
@@ -1,19 +1,33 @@
-import type { SpawnOptions } from "bun"
+import type { SpawnOptions } from 'bun';
 
 const spawnOptions: SpawnOptions.OptionsObject = {
-    stdin: "inherit",
-    stdout: "inherit",
-    stderr: "inherit",
-}
+	stdin: 'inherit',
+	stdout: 'inherit',
+	stderr: 'inherit',
+};
 
 const run = async () => {
-    Bun.spawn(["nx", "serve", "frontend-portal"], spawnOptions)
-    Bun.spawn(["nx", "serve", "backend-portal"], spawnOptions)
-    // Bun.spawn(["nx", "serve-static", "backend-portal"], spawnOptions)
+	Bun.spawn(
+		[
+			'nx',
+			'serve',
+			'frontend-portal',
+		],
+		spawnOptions,
+	);
+	Bun.spawn(
+		[
+			'nx',
+			'serve',
+			'backend-portal',
+		],
+		spawnOptions,
+	);
+	// Bun.spawn(["nx", "serve-static", "backend-portal"], spawnOptions)
 
-    process.on("SIGINT", async () => {
-        console.log("Cleaning up...")
-    })
-}
+	process.on('SIGINT', async () => {
+		console.log('Cleaning up...');
+	});
+};
 
-run()
+run();
