11e8eb68e1c15286131666ee63097d0414adfb2f
rj <rujorgensen@gmail.com>
2026-01-15 10:51:29 +0100

    chore: update Prisma commands to use Bun CLI and adjust environment variable access

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,6 +1,4 @@
-import 'dotenv/config';
 import type { PrismaConfig } from 'prisma';
-import { env } from 'prisma/config';
 
 export default {
     schema: 'schema.prisma',
@@ -8,6 +6,6 @@ export default {
         path: 'migrations',
     },
     datasource: {
-        url: env('FLUX_DATABASE_URL'),
+        url: Bun.env['FLUX_DATABASE_URL'],
     },
 } satisfies PrismaConfig;
