121f2ecd86e26253275b758d0fb0888c57b31b20
rj <rujorgensen@gmail.com>
2026-05-15 15:06:48 +0200

    .

--- libs/portal/backend/auth/src/lib/auth.ts
+++ libs/portal/backend/auth/src/lib/auth.ts
@@ -3,12 +3,16 @@ import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '@prisma-types/flux';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-if (!process.env['GOOGLE_CLIENT_ID']) {
-    throw new Error('GOOGLE_CLIENT_ID is not set in environment variables');
-}
+if (!process.env['FLUX_MASTER_PASSWORD']) {
+    if (!process.env['GOOGLE_CLIENT_ID']) {
+        throw new Error('GOOGLE_CLIENT_ID is not set in environment variables');
+    }
 
-if (!process.env['GOOGLE_CLIENT_SECRET']) {
-    throw new Error('GOOGLE_CLIENT_SECRET is not set in environment variables');
+    if (!process.env['GOOGLE_CLIENT_SECRET']) {
+        throw new Error('GOOGLE_CLIENT_SECRET is not set in environment variables');
+    }
+} else {
+    console.log(`Master password login enabled.`);
 }
 
 if (!process.env['FLUX_DATABASE_URL']) {
@@ -47,6 +51,9 @@ export const auth = betterAuth({
         'http://localhost:3001',
         'http://localhost:9000',
     ],
+    emailAndPassword: {
+        enabled: true,
+    },
     socialProviders: {
         google: {
             clientId: process.env['GOOGLE_CLIENT_ID'] as string,
